bd84618aebaed30bdceb12413f6c4078ea4eb79b
Joan Zapata <jzapata@excilys.com>
2013-05-01 13:56:49 +0200

    First commit, initialized project with Yeoman for angular js, added a navbar and a macbook pro

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('joanzapata.frApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
