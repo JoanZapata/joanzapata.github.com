78aba565ceb67d3a043bcddde7fb33adf3d40eb3
Joan <zapata.joan@gmail.com>
2014-05-24 13:51:51 +0200

    Fix build with font awesome

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,7 +257,8 @@ module.exports = function(grunt) {
                             '.htaccess',
                             'images/{,*/}*.{webp,gif}',
                             'styles/fonts/{,*/}*.*',
-                            'bower_components/sass-bootstrap/fonts/*.*'
+                            'bower_components/sass-bootstrap/fonts/*.*',
+                            'bower_components/fontawesome/fonts/*.*'
                         ]
                     }
                 ]
