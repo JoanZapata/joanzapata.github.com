6592c2360c35a39789f871cea5142331bd6999cb
Joan <zapata.joan@gmail.com>
2014-12-17 10:30:03 +0100

    Added 'more' on each experience to attract clicks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,38 +14,40 @@ $(function() {
 });
 
 
-var ANIMATION_DURATION = 300;
+var ANIMATION_DURATION = 250;
 var ANIMATION_SIDE = 30;
 
 // Toggle visibility of a card
 function togglecard(cardId) {
 
     // Get the card
-    var cardElem = $("#" + cardId);
+    var cardElem = $("#card-" + cardId);
 
     // Use the immediate child as a height reference
     var child = cardElem.children().first();
+    var more = $("#more-" + cardId);
     var containerHeight = child.outerHeight();
 
     // Check if card is displayed
     cardElem.toggleClass("card-displayed");
     if (cardElem.hasClass("card-displayed")) {
-        cardElem.animate({height: containerHeight}, ANIMATION_DURATION);
+        cardElem.transition({height: containerHeight}, ANIMATION_DURATION);
         child.css({left: ANIMATION_SIDE});
-        child.animate({left: 0, opacity: 1}, ANIMATION_DURATION);
-
+        child.transition({left: 0, opacity: 1}, ANIMATION_DURATION);
+        if (more) more.transition({opacity: 0}, ANIMATION_DURATION);
 
         // If open, close others
         $(".card-displayed").each(function() {
             var id = $(this).attr("id");
-            if (id != cardId) {
-                togglecard(id);
+            if (id != "card-" + cardId) {
+                togglecard(id.substring(5));
             }
         });
 
     } else {
-        child.animate({left: ANIMATION_SIDE, opacity: 0}, ANIMATION_DURATION);
-        cardElem.animate({height: 0}, ANIMATION_DURATION);
+        child.transition({left: ANIMATION_SIDE, opacity: 0}, ANIMATION_DURATION);
+        cardElem.transition({height: 0}, ANIMATION_DURATION);
+        if (more) more.transition({opacity: 1}, ANIMATION_DURATION);
     }
 
 }
