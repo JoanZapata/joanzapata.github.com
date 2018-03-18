5fa87bc5b349e83b24ee1ec765b729c647691285
Joan Zapata <zapata.joan@gmail.com>
2018-03-18 16:07:58 +0100

    Animate the smoke, the lamp, the plant, and set the time on the clock

--- assets/main.js
+++ assets/main.js
@@ -11,8 +11,6 @@ const openScreenshot = screenshot => {
 
 // Toggle visibility of a card
 const togglecard = cardId => {
-  console.log("Alo?");
-
   // Get the card
   var cardElem = $("#card-" + cardId);
   var titleElem = $("#title-" + cardId);
@@ -87,3 +85,62 @@ const showMoreBooks = () => {
   $('#moreBooks').toggleClass('d-none')
   $('#showMoreBooks').toggleClass('d-none')
 }
+
+$(() => {
+  // Randomize a number between 0.7 and 1.3 of the given value.
+  // Used to make animations less predictible.
+  let randomize = (num) => {
+    return (num * .7) + (Math.random() * num * .6);
+  }
+
+  // Animate The smoke on the cup
+  let smoke = Snap.select('#smoke1');
+  let smoke2 = Snap.select('#smoke2');
+  let smoke3 = Snap.select('#smoke3');
+  let smokeShape1 = smoke.node.getAttribute('d');
+  let smokeShape2 = smoke2.node.getAttribute('d');
+  let smokeShape3 = smoke3.node.getAttribute('d');
+  const SMOKE_ANIMATION_DURATION = 4000
+  let smokeAnimation = () => {
+    smoke.animate({d: smokeShape2}, randomize(SMOKE_ANIMATION_DURATION/3), () => {
+      smoke.animate({d: smokeShape3}, randomize(SMOKE_ANIMATION_DURATION/3), () => {
+        smoke.animate({d: smokeShape1}, randomize(SMOKE_ANIMATION_DURATION/3), smokeAnimation);
+      });
+    });
+  }
+  smokeAnimation();
+
+  // Animate the plant
+  let plant = Snap.select('#plant');
+  const PLANT_ANIMATION_DURATION = 3000
+  let plantAnimation = () => {
+    let matrix = new Snap.Matrix();
+    matrix.skewX(randomize(-2));
+    plant.animate({transform: matrix}, randomize(PLANT_ANIMATION_DURATION/2), () => {
+      var matrix = new Snap.Matrix();
+      matrix.skewX(0);
+      plant.animate({transform: matrix}, randomize(PLANT_ANIMATION_DURATION/2), plantAnimation);
+    });
+  };
+  plantAnimation();
+
+  // Lamp animation
+  let lamp = Snap.select('#lamp');
+  let lampAnimation = () => {
+    lamp.animate({transform: 'r6,20,10'}, 600, mina.easeinout, () => {
+      setTimeout(() => {
+          lamp.animate({transform: 'r0,20,10'}, 600, mina.easeinout, () => { setTimeout(lampAnimation, randomize(5000)) });
+      }, randomize(3000));
+    });
+  }
+  lampAnimation();
+
+  // Set the time on the clock
+  let hourClock = Snap.select('#hourClock');
+  let minuteClock = Snap.select('#minuteClock');
+  let date = new Date();
+  let hours = date.getHours() % 12;
+  let minutes = date.getMinutes();
+  hourClock.attr({transform: `r${hours / 12 * 360},37.5,37.5`});
+  minuteClock.attr({transform: `r${minutes / 60 * 360},37.5,37.5`});
+});
